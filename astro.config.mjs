058d9230b837f6e18b228c8afba5b54725dea913
Divya Jayaraman <divyajayaram1210@gmail.com>
2025-05-19 13:43:03 +0530

    Prepare for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react"; // ✅ Add this
-import image from '@astrojs/image';
+
 
 export default defineConfig({
-  integrations: [tailwind(), react(),image()],
+  integrations: [tailwind(), react()],
     
 });
